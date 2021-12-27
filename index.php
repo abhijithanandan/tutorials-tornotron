2dad0857fd8bdd7b6d597be573bb1feafab5004e
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2021-12-27 22:57:43 +0530

    Added main container div elements to index.php

--- index.php
+++ index.php
@@ -8,5 +8,17 @@
 </head>
 <body>
   <h1>Official website of Grand Kerala</h1>
+
+  <div class="row">
+    <div class="container">
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam perspiciatis quam quisquam accusamus labore veniam, laboriosam ipsum, facere iste dolor officiis, ipsam voluptates hic a ab impedit qui nulla minima.</p>
+    </div>
+    <div class="container">
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ipsam magnam, quibusdam excepturi corrupti, veritatis ex similique enim eaque officiis quas aspernatur animi accusantium quia tempore qui dicta nostrum et.</p>
+    </div>
+    <div class="container">
+      <p>psum dolor sit amet consectetur adipisicing elit. Voluptatibus praesentium facilis eaque adipisci reiciendis quasi perferendis. Nemo, enim animi voluptate commodi, beatae voluptatum vel porro ut, temporibus dignissimos perferendis quas!</p>
+    </div>
+  </div>
 </body>
 </html>
