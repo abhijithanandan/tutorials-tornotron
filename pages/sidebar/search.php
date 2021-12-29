66ef9c85cc84240567e24dd65267bf7db724581a
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2021-12-29 21:41:00 +0530

    Added more code to search.php

--- pages/sidebar/search.php
+++ pages/sidebar/search.php
@@ -7,6 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus accusantium sapiente facilis, nihil laboriosam commodi itaque, vel deserunt molestias rem quo quas veritatis ut facere. Iure voluptatem similique molestiae pariatur!</p> 
 </body>
 </html>
