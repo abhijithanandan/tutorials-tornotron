72e7c5df89b28390d77c6adb2c6dff4fd38b9762
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2021-12-29 21:56:51 +0530

    Modified search.php in sidebar

--- pages/sidebar/search.php
+++ pages/sidebar/search.php
@@ -7,6 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus accusantium sapiente facilis, nihil laboriosam commodi itaque, vel deserunt molestias rem quo quas veritatis ut facere. Iure voluptatem similique molestiae pariatur!</p> 
+   <p>Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id voluptate nostrum et cupiditate. Numquam ratione maxime nemo sed officia consectetur fuga quam, odio corporis accusantium voluptates asperiores sit id a. sit amet consectetur adipisicing elit. Temporibus accusantium sapiente facilis, nihil laboriosam commodi itaque, vel deserunt molestias rem quo quas veritatis ut facere. Iure voluptatem similique molestiae pariatur!</p> 
 </body>
 </html>
