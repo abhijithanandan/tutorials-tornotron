c372a72876671927e5c51804ff0bea4b45b820b2
Gautam Sankar <agsankar96@gmail.com>
2022-01-13 21:49:09 +0530

    Added some text in main contents

--- pages/sidebar/body/main-content.php
+++ pages/sidebar/body/main-content.php
@@ -7,6 +7,7 @@
     <title>Document</title>
 </head>
 <body>
+    <h1>We have created footer</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium quae fugiat at incidunt minus dicta sed hic esse voluptatibus sit officiis quidem corrupti neque totam cum, mollitia doloremque ut laborum?</p>
 </body>
 </html>
